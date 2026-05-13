e5fc475ddec4cd189001d3175ab3d0540d755da1
Veeresh5645 <veereshvagdale555@gmail.com>
2026-05-13 18:26:51 +0530

    fixed student_routes.py

--- backend/routes/student_routes.py
+++ backend/routes/student_routes.py
@@ -3,14 +3,11 @@ from flask import Blueprint, request, jsonify
 from database.db import db
 
 from models.student_model import Student
-from models.fee_structure_model import FeeStructure
-from models.bus_route_model import BusRoute
 
 student_bp = Blueprint(
     'students',
     __name__
 )
-
 
 # GET ALL STUDENTS
 @student_bp.route(
@@ -19,52 +16,23 @@ student_bp = Blueprint(
 )
 def get_students():
 
-    try:
+    students = Student.query.all()
 
-        students = Student.query.all()
+    output = []
 
-        output = []
+    for student in students:
 
-        for student in students:
+        output.append({
 
-            output.append({
+            "id": student.id,
 
-                "id": student.id,
+            "full_name": student.full_name,
 
-                "full_name": student.full_name,
+            "admission_number":
+                student.admission_number
+        })
 
-                "admission_number":
-                    student.admission_number,
-
-                "joining_year":
-                    student.joining_year,
-
-                "academic_year":
-                    student.academic_year,
-
-                "current_class":
-                    student.current_class,
-
-                "paid_amount":
-                    student.paid_amount,
-
-                "remaining_amount":
-                    student.remaining_amount,
-
-                "total_fee":
-                    student.total_fee
-            })
-
-        return jsonify(output)
-
-    except Exception as e:
-
-        print("GET STUDENT ERROR:")
-        print(e)
-
-        return jsonify({
-            "error": str(e)
-        }), 500
+    return jsonify(output)
 
 
 # ADD STUDENT
@@ -74,182 +42,28 @@ def get_students():
 )
 def add_student():
 
-    try:
+    data = request.get_json()
 
-        data = request.get_json()
+    print(data)
 
-        print("DATA RECEIVED:")
-        print(data)
+    student = Student(
 
-        fee_structure = FeeStructure.query.filter_by(
+        full_name=data.get(
+            'full_name'
+        ),
 
-            academic_year=data.get(
-                'academic_year'
-            ),
-
-            class_name=data.get(
-                'current_class'
-            )
-
-        ).first()
-
-        tuition_fee = 0
-        admission_fee = 0
-        bus_fee = 0
-
-        if fee_structure:
-
-            tuition_fee = (
-                fee_structure.tuition_fee
-            )
-
-            if data.get(
-                'admission_type'
-            ) == 'new':
-
-                admission_fee = (
-                    fee_structure
-                    .new_admission_fee
-                )
-
-            else:
-
-                admission_fee = (
-                    fee_structure
-                    .old_admission_fee
-                )
-
-        if data.get('bus_required'):
-
-            route = BusRoute.query.filter_by(
-
-                route_name=data.get(
-                    'bus_route'
-                )
-
-            ).first()
-
-            if route:
-
-                bus_fee = route.bus_fee
-
-        old_due_total = 0
-
-        for due in data.get(
-            'old_dues',
-            []
-        ):
-
-            old_due_total += int(
-
-                due.get(
-                    'pending_fee',
-                    0
-                ) or 0
-            )
-
-        total_fee = (
-
-            int(tuition_fee or 0)
-
-            +
-
-            int(admission_fee or 0)
-
-            +
-
-            int(bus_fee or 0)
-
-            +
-
-            old_due_total
+        admission_number=data.get(
+            'admission_number'
         )
+    )
 
-        paid_amount = int(
+    db.session.add(student)
 
-            data.get(
-                'paid_amount',
-                0
-            ) or 0
-        )
+    db.session.commit()
 
-        remaining_amount = (
-            total_fee - paid_amount
-        )
+    return jsonify({
 
-        student = Student(
+        "message":
+            "Student added successfully"
 
-            full_name=data.get(
-                'full_name'
-            ),
-
-            admission_number=data.get(
-                'admission_number'
-            ),
-
-            admission_type=data.get(
-                'admission_type'
-            ),
-
-            joining_year=data.get(
-                'joining_year'
-            ),
-
-            academic_year=data.get(
-                'academic_year'
-            ),
-
-            current_class=data.get(
-                'current_class'
-            ),
-
-            section=data.get(
-                'section'
-            ),
-
-            parent_name=data.get(
-                'parent_name'
-            ),
-
-            phone=data.get(
-                'phone'
-            ),
-
-            address=data.get(
-                'address'
-            ),
-
-            bus_required=data.get(
-                'bus_required'
-            ),
-
-            bus_route=data.get(
-                'bus_route'
-            ),
-
-            total_fee=total_fee,
-
-            paid_amount=paid_amount,
-
-            remaining_amount=remaining_amount
-        )
-
-        db.session.add(student)
-
-        db.session.commit()
-
-        return jsonify({
-
-            "message":
-                "Student added successfully"
-
-        }), 201
-
-    except Exception as e:
-
-        print("STUDENT ADD ERROR:")
-        print(e)
-
-        return jsonify({
-            "error": str(e)
-        }), 500
+    }), 201
